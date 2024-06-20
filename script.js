87d478e96526fe61714ecf7eed87a55b6adea7de
xybelle <150109343+xybelle@users.noreply.github.com>
2024-06-20 17:57:52 +0800

    Add grid creation function with rows and squares
    Implement createGrid function to generate a grid with 16 rows and 16 columns. Each row and square are styled using CSS classes

---
+++ script.js
@@ -0,0 +1,18 @@
+const grid = document.querySelector(".container");
+
+function createGrid() {
+    const numberOfRows = 16;
+    const column = 16;
+    for (let i = 0; i < numberOfRows; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        grid.appendChild(row);
+        for (let j = 0; j < column; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            row.appendChild(square);
+        }
+    }
+}
+
+createGrid();

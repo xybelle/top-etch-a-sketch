459e45d541f35d5abd864b6a6cfaedb58b8ea6ed
xybelle <150109343+xybelle@users.noreply.github.com>
2024-09-18 16:29:29 +0800

    Add function to get number input, square color stays

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 const grid = document.querySelector(".grid-container");
 
-function createGrid() {
-    const numberOfRows = 16;
-    const column = 16;
-    for (let i = 0; i < numberOfRows; i++) {
+function createGrid(gridSize = 16) {
+    for (let i = 0; i < gridSize; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
         grid.appendChild(row);
-        for (let j = 0; j < column; j++) {
+        for (let j = 0; j < gridSize; j++) {
             const square = document.createElement("div");
             square.classList.add("square");
             row.appendChild(square);
+
+            square.addEventListener("mouseover", () => {
+                square.classList.add("active");
+            })
         }
     }
+}
+
+const btn = document.querySelector("#btn");
+btn.addEventListener("click",() => {
+    let newSize = getNumber();
+    removeGrid();
+    createGrid(newSize, newSize);
+})
+
+function getNumber() {
+    let number;
+    do {
+        number = prompt("Please enter an integer between 1 and 100:");
+        if (number === null) return 16;
+    } while (number < 1 || number > 100);
+    return number;
+}
+
+function removeGrid() {
+    grid.innerHTML = "";
 }
 
 createGrid();
